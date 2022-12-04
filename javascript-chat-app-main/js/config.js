1ad2d32d0266127f9ed90131064ddeeed6d5c187
rajwardhanspimple <85007388+rajwardhanspimple@users.noreply.github.com>
2022-12-04 11:32:47 +0530

    Add files via upload

---
+++ javascript-chat-app-main/js/config.js
@@ -0,0 +1,16 @@
+const config = {
+  apiKey: "AIzaSyADKazuF03bc1f8-OyUWAzqUvBNYOWR7sI",
+  authDomain: "klipy-8dcc3.firebaseapp.com",
+  databaseURL: "https://klipy-8dcc3-default-rtdb.firebaseio.com",
+  projectId: "klipy-8dcc3",
+  storageBucket: "klipy-8dcc3.appspot.com",
+  messagingSenderId: "650383358053",
+  appId: "1:650383358053:web:ee67e36cd798ed3b6fee8c",
+  measurementId: "G-8375R01Y6B"
+    CometChatAppId: 2270273335cb5216,
+    CometChatRegion: us,
+    CometChatAuthKey: 5f81f1499bfcef7e75fa226126a39a96b3e958c7,
+    CometChatAPIKey: 37b2bba44735888a67072fbcf599914c21a0b418,
+    CometChatWidgetId: 5f81f1499bfcef7e75fa226126a39a96b3e958c7
+  };
+  
